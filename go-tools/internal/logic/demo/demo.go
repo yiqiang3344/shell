9b3065caa906b482418dcedcc7059cb04edb6f74
junqiang.yi <junqiang.yi2@xinfei.cn>
2024-07-09 17:45:59 +0800

    完善demo，区分更好的区分参数设置中Orphan的作用。

--- go-tools/internal/logic/demo/demo.go
+++ go-tools/internal/logic/demo/demo.go
@@ -18,5 +18,7 @@ func init() {
 }
 
 func (s *sDemo) Demo(ctx context.Context, parser *gcmd.Parser) {
+	g.DumpWithType("argsA", parser.GetOpt("argsA"))
+	g.DumpWithType("argsB", parser.GetOpt("argsB").IsNil())
 	g.Dump(parser.GetOptAll(), parser.GetArgAll())
 }
